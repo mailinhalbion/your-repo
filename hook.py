003e7f5f2c798a9776cfac037faca256b63adef4
mrcuongryan <mrcuongvietnam@gmail.com>
2023-12-31 20:04:50 +0700

    ok

--- hook.py
+++ hook.py
@@ -1,46 +1,53 @@
 from flask import Flask, request, jsonify
 import subprocess
+from subprocess import CalledProcessError
 
 app = Flask(__name__)
 
 @app.route('/webhook-endpoint', methods=['POST'])
 def webhook():
-    data = request.json
-
-    # Extract relevant information from the payload
-    ref = data.get('ref', '')
-    commits = data.get('commits', [])
-    sender = data.get('sender', {})
-    
-    print(f"Received a push to branch: {ref}")
-    
-    for commit in commits:
-        commit_id = commit.get('id', '')
-        message = commit.get('message', '')
-        author = commit.get('author', {}).get('name', '')
-
-        print(f"  Commit ID: {commit_id}")
-        print(f"  Author: {author}")
-        print(f"  Message: {message}")
-        print("")
-
-    print(f"Push triggered by: {sender.get('login', '')}")
-
-    # Perform git pull
     try:
+        data = request.json
+
+        # Extract relevant information from the payload
+        ref = data.get('ref', '')
+        commits = data.get('commits', [])
+        sender = data.get('sender', {})
+        
+        print(f"Received a push to branch: {ref}")
+        
+        for commit in commits:
+            commit_id = commit.get('id', '')
+            message = commit.get('message', '')
+            author = commit.get('author', {}).get('name', '')
+
+            print(f"  Commit ID: {commit_id}")
+            print(f"  Author: {author}")
+            print(f"  Message: {message}")
+            print("")
+
+        print(f"Push triggered by: {sender.get('login', '')}")
+
+        # Perform git pull
         subprocess.run(['git', 'pull'])
         print("Git pull successful.")
-    except Exception as e:
-        print(f"Error during git pull: {e}")
 
-    # Perform git pull
+        # Restart the Flask application after git pull
+        restart_flask()
+
+        return jsonify({'status': 'success'}), 200
+
+    except Exception as e:
+        print(f"Error during webhook processing: {e}")
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+
+def restart_flask():
     try:
-        subprocess.run(['python3', 'app.py'])
-        print("run app.py successful.")
-    except Exception as e:
-        print(f"Error during run app.py: {e}")
-
-    return jsonify({'status': 'success'}), 200
+        # Restart the Flask application
+        subprocess.run(['python3', 'hook.py'])
+        print("Restarted Flask application successfully.")
+    except CalledProcessError as e:
+        print(f"Error during Flask application restart: {e}")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
