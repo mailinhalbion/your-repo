6855fa04636ef5280724f5bf4af8d32392d7c87b
mrcuongryan <mrcuongvietnam@gmail.com>
2024-01-01 11:48:34 +0700

    ok

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import asyncio
 from telegram import Bot
 
 # Thay thế 'YOUR_BOT_TOKEN' bằng token của bot bạn
 bot_token = '6175232239:AAHJ9fyetFQuqVtvlz7vpoezJZvQrXWYYe4'
 chat_id = '-1002110020789'  # Thay thế 'TARGET_CHAT_ID' bằng ID của cuộc trò chuyện bạn muốn gửi tin nhắn đến
 
-def send_message(message):
+async def send_message(message):
     bot = Bot(token=bot_token)
-    bot.send_message(chat_id=chat_id, text=message)
+    await bot.send_message(chat_id=chat_id, text=message)
 
-# Gọi hàm để gửi tin nhắn
-send_message("Xin chào, đây là tin nhắn tự động từ bot Telegram!")
+# Chạy event loop và gọi hàm để gửi tin nhắn
+loop = asyncio.get_event_loop()
+loop.run_until_complete(send_message("Xin chào, đây là tin nhắn tự động từ bot Telegram!"))
+
